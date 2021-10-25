ea20eacd191e8d831ff5a11d4111c62d5e2fb8a7
Dzmitry Lyshko <lyshko.dmitry@gmail.com>
2021-10-25 15:10:51 +0300

    Add unit testing project

---
+++ test/specs/add.spec.js
@@ -0,0 +1,38 @@
+const Calculator = require('../../app/calculator');
+const {expect} = require('chai');
+
+describe('add', function() {
+  let calc;
+
+  beforeEach(function() {
+    calc = new Calculator();
+  });
+
+  afterEach(function() {
+    calc = null;
+  });
+
+  it('should return 6 when called with numbers 2 and 4', function() {
+    expect(calc.add(2, 4)).to.be.equal(6);
+  });
+
+  it('should return -6 when called with numbers -2 and -4', function() {
+    expect(calc.add(-2, -4)).to.be.equal(-6);
+  });
+
+  it('should return 23 when called with numbers 23 and 0', function() {
+    expect(calc.add(23, 0)).to.be.equal(23);
+  });
+
+  it('should return -42 when called with numbers 0 and -42', function() {
+    expect(calc.add(0, -42)).to.be.equal(-42);
+  });
+
+  it('should return 55 when called with numbers 77 and -22', function() {
+    expect(calc.add(77, -22)).to.be.equal(55);
+  });
+
+  it('should return -16 when called with numbers 24 and -40', function() {
+    expect(calc.add(24, -40)).to.be.equal(-16);
+  });
+});
---
+++ test/specs/multiply.spec.js
@@ -0,0 +1,30 @@
+const Calculator = require('../../app/calculator');
+const {expect} = require('chai');
+
+describe('multiply', function() {
+  let calc;
+
+  beforeEach(function() {
+    calc = new Calculator();
+  });
+
+  afterEach(function() {
+    calc = null;
+  });
+
+  it('should return 8 when called with numbers 2 and 4', function() {
+    expect(calc.multiply(2, 4)).to.be.equal(8);
+  });
+
+  it('should return -15 when called with numbers -3 and 5', function() {
+    expect(calc.multiply(-3, 5)).to.be.equal(-15);
+  });
+
+  it('should return 0 when called with numbers -11 and 0', function() {
+    expect(calc.multiply(-11, 0)).to.be.equal(0);
+  });
+
+  it('should return 0 when called with numbers 7 and 0', function() {
+    expect(calc.multiply(7, 0)).to.be.equal(0);
+  });
+});
